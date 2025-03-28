a973a033dedb39a424a94d2f57e5de82f271f4c0
bokney <toknyk@gmail.com>
2025-03-28 23:19:06 +0000

    sample api responses

---
+++ test/mock_api_responses.py
@@ -0,0 +1,189 @@
+
+MOCK_GET_OPEN_ORDERS = [
+    {
+        "account": {
+            "borrowMakingAmount": "0",
+            "createdAt": "2024-10-25 10:56:11+00:00",
+            "expiredAt": "2025-01-23 10:56:09+00:00",
+            "makingAmount": "20773185103",
+            "oriMakingAmount": "20773185103",
+            "oriTakingAmount": "195697737",
+            "takingAmount": "195697737",
+            "uniqueId": 6912831984773988945,
+            "updatedAt": "2024-10-25 10:56:11+00:00",
+            "feeAccount":
+                "Apg7FGC1MSjVewHzbpvvA72vMKNiApRVCoWztGUYcYy8",
+            "inputMint":
+                "8kp7gVCTWVbNP257A7MHgsYheshqYGnqWbApS6f3pump",
+            "inputMintReserve":
+                "G4hCavWE96sP9Dm7vGbQRVxGbPaZFr65kahJEm7qpvqt",
+            "inputTokenProgram":
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            "maker":
+                "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+            "outputMint":
+                "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+            "outputTokenProgram":
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            "feeBps": 81,
+            "bump": 255
+        },
+        "publicKey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt"
+    }, {
+        "account": {
+            "borrowMakingAmount": "0",
+            "createdAt": "2024-10-31 14:25:03+00:00",
+            "expiredAt": "2025-01-29 14:25:01+00:00",
+            "makingAmount": "4618000000",
+            "oriMakingAmount": "4618000000",
+            "oriTakingAmount": "25032746",
+            "takingAmount": "25032746",
+            "uniqueId": 9502199936590349991,
+            "updatedAt": "2024-10-31 14:25:03+00:00",
+            "feeAccount":
+                "Apg7FGC1MSjVewHzbpvvA72vMKNiApRVCoWztGUYcYy8",
+            "inputMint":
+                "BuxH23osRyFFLbWG3czrTsfBQYbxzVZ8f7QV4cjTHN5x",
+            "inputMintReserve":
+                "FWsRUDoRNU5o2yD2i8HD5r2zDdoDV6VUPzyGayQsHRRL",
+            "inputTokenProgram":
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            "maker":
+                "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+            "outputMint":
+                "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+            "outputTokenProgram":
+                "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+            "feeBps": 81,
+            "bump": 255
+        },
+        "publicKey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt"
+    }
+]
+
+MOCK_GET_ORDER_HISTORY_PAGE_1 = {
+        "orders": [{
+            "userPubkey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+            "orderKey": "227SajBVJMBedunrjxWnjYnirCvbeEWepHFrtbeCkJo8",
+            "inputMint": "6ogzHhzdrQr9Pgv6hZ2MNze7UrzBMAFyBBWUYp1Fhitx",
+            "outputMint": "So11111111111111111111111111111111111111112",
+            "makingAmount": "4080",
+            "takingAmount": "3.264",
+            "remainingMakingAmount": "4080",
+            "remainingTakingAmount": "3.264",
+            "expiredAt": "2024-03-07 20:56:36+00:00",
+            "createdAt": "2024-03-04 20:56:49+00:00",
+            "updatedAt": "2024-03-05 17:40:04+00:00",
+            "status": "Cancelled",
+            "openTx": (
+                "2FSVxwfnRrMGnggbfAkzWcrEvWwVnQ2rPmoZmqoaqJ4p"
+                "YMpcLuJN1Ac9h5YKyKwS8Fw9YqQ73Zbdr8xAVCTNPuc6"
+            ),
+            "closeTx": (
+                "3CdyhHZ56xmNbzVBGTpZuGo9R31bNmKmaJwssm52tTq2"
+                "jGtsmwWiHd6SyJ8S572NEGrBq96zFwLbGYzzwNKWLXVs"
+            ),
+            "programVersion":
+                "jupoNjAxXgZ4rjzxzPMP4oxduvQsQtZzyknqvzYNrNu",
+            "trades": []
+        }, {
+            "userPubkey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+            "orderKey": "DBZth2NC4E5a2kxG9scwpTKfRKgw9MUebAhmKyNzYf2L",
+            "inputMint": "6ogzHhzdrQr9Pgv6hZ2MNze7UrzBMAFyBBWUYp1Fhitx",
+            "outputMint": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+            "makingAmount": "1110",
+            "takingAmount": "33.744",
+            "remainingMakingAmount": "1110",
+            "remainingTakingAmount": "33.744",
+            "expiredAt": "2024-03-09 11:57:42+00:00",
+            "createdAt": "2024-03-06 11:57:47+00:00",
+            "updatedAt": "2024-03-06 11:59:15+00:00",
+            "status": "Cancelled",
+            "openTx": (
+                "8u1JbJE3LuMk2DrtmKzcDBvU21MemvSRkw8vZvstu9jH"
+                "5rR86pNPJHrn15c69kAsa7WuuAFhiF3X9Rv4LdvATgK"
+            ),
+            "closeTx": (
+                 "37S6cJ43VXTRJu12R8fj2hbL3kTf3DeCKEB16K2P8hun"
+                 "Ko6fY8JGZsVt16kEmbCLZunBr4cTF79huQJSXS2pw3C2"
+                ),
+            "programVersion": "jupoNjAxXgZ4rjzxzPMP4oxduvQsQtZzyknqvzYNrNu",
+            "trades": []
+        }],
+        "hasMoreData": "true",
+        "page": 1
+    }
+
+MOCK_GET_ORDER_HISTORY_PAGE_2 = {"orders": [{
+        "userPubkey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+        "orderKey": "2Y2g4DimTfAaSc95B5GGYAVz8nhUB2X7p21uUTY226Jd",
+        "inputMint": "6ogzHhzdrQr9Pgv6hZ2MNze7UrzBMAFyBBWUYp1Fhitx",
+        "outputMint": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        "makingAmount": "1665",
+        "takingAmount": "67.4325",
+        "remainingMakingAmount": "1665",
+        "remainingTakingAmount": "67.4325",
+        "expiredAt": "2024-03-09 11:58:13+00:00",
+        "createdAt": "2024-03-06 11:58:19+00:00",
+        "updatedAt": "2024-03-06 11:59:16+00:00",
+        "status": "Cancelled",
+        "openTx": (
+            "61QcX25PdpizZeNXgzqEKA9NJCbzzdDmGbK4tnh4oSAq"
+            "1yQ2xv5dLMob9greVuiQPTVD24Fve3ktXYPF4sdRXPUV"
+        ),
+        "closeTx": (
+            "4PgfgJutdThx3fGVgoiwcHMsPvKvKmm7K8v6tz5B9sA8"
+            "zNnyHkwd4k1LjrGDy2WLSPMfVYt3s2PUPdhAWVe594F"
+        ),
+        "programVersion": "jupoNjAxXgZ4rjzxzPMP4oxduvQsQtZzyknqvzYNrNu",
+        "trades": []
+    }, {
+        "userPubkey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+        "orderKey": "E5jxerZiyoXNJo39LFthr7VK1N8nuRgSHgo6atqY3gJZ",
+        "inputMint": "6ogzHhzdrQr9Pgv6hZ2MNze7UrzBMAFyBBWUYp1Fhitx",
+        "outputMint": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        "makingAmount": "2220",
+        "takingAmount": "119.88",
+        "remainingMakingAmount": "2220",
+        "remainingTakingAmount": "119.88",
+        "expiredAt": "2024-03-09 11:58:28+00:00",
+        "createdAt": "2024-03-06 11:58:35+00:00",
+        "updatedAt": "2024-03-06 11:59:21+00:00",
+        "status": "Cancelled",
+        "openTx": (
+            "5WspKtSX2KH2JFrG57eVR5xYP5h2ZreVKnNxK6yGUWX6"
+            "2tq3uoTzD8UjjcaAUsM6WmTXsMLuvgR43e6PKjoApzg6"
+        ),
+        "closeTx": (
+            "5BGAavVUoy2Vxv3U5RQNX4F3K6QioXxZV8qwDUoX23hF"
+            "aBcaJYGVUUa4XxV4ZHvCiahY3f51yQFYiMAkc6ZRw4XU"
+        ),
+        "programVersion": "jupoNjAxXgZ4rjzxzPMP4oxduvQsQtZzyknqvzYNrNu",
+        "trades": []
+    }, {
+        "userPubkey": "B2QXpgPZA1FAxcNwfe2SdBeTkNeMqhVPurpoUxQ9P1Qt",
+        "orderKey": "bAR3BuQ942rmKX1qvYmU1drDtYGKuFRLshvWAQCmtDH",
+        "inputMint": "6ogzHhzdrQr9Pgv6hZ2MNze7UrzBMAFyBBWUYp1Fhitx",
+        "outputMint": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        "makingAmount": "2775",
+        "takingAmount": "199.8",
+        "remainingMakingAmount": "2775",
+        "remainingTakingAmount": "199.8",
+        "expiredAt": "2024-03-09 11:58:45+00:00",
+        "createdAt": "2024-03-06 11:58:48+00:00",
+        "updatedAt": "2024-03-06 11:59:14+00:00",
+        "status": "Cancelled",
+        "openTx": (
+            "ZdX4mv56EJ699f3ee8xkNSJXZVdE3HMVEa2Wakza4mmP"
+            "vkvn4zi4n4ucLpHqxHt5V5eqDhb3V7zvwc3Hg57QWWr"
+        ),
+        "closeTx": (
+            "r4EBJxQqbv289mzTwnq5RTb5A2Czqjd4QaVUyRjMXYkn"
+            "tttL9P8TGtVYvsPYHBk6E2s3wXNaCHMC8uhASku3e3r"
+        ),
+        "programVersion": "jupoNjAxXgZ4rjzxzPMP4oxduvQsQtZzyknqvzYNrNu",
+        "trades": []
+    }],
+        "hasMoreData": "false",
+        "page": 1
+    }
